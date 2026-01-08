79fee25ec95a764fce425bc6ac47b1eb2c7b468d
Sanika Mogal <sanikamogal925@gmail.com>
2026-01-08 23:56:26 +0530

    Homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,28 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import {callbackify} from "node:util";
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "RM – Analyze. Improve. Get Hired" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg -cover">
+    <Navbar />
+    <section>
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback</h2>
+      </div>
+    </section>
+
+    {resumes.map(resume => (
+        <div>
+          <h1>{resume.jobTitle}</h1>
+        </div>
+    ))}
+  </main>;
 }
